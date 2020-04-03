fc3a5efc60c74cd39bc80cf8ddaa437bf511c6e9
Lluis Santamaria <lluis.santamariasola@mastercard.com>
2020-04-03 15:08:29 +0100

    change server port

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,5 +1,5 @@
 export class Constants {
-    public static readonly API_URL = "http://localhost:8081";
+    public static readonly API_URL = "http://localhost:8080";
     public static readonly CREATE_EVENT_URL = Constants.API_URL + "/events";
     public static readonly GET_EVENTS_STREAM_URL = Constants.API_URL + "/events";
     public static readonly GET_CURRENCIES_URL = Constants.API_URL + "/currencies";
